aa43db2c33a6aabcabba531c37083c3dbbfa94e1
reymyr <reyhanemyr51@gmail.com>
2021-11-03 16:31:49 +0700

    add ElGamal and ECC to frontend and backend

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, request, send_file
-from RSA import RSA
+from rsa import RSA
 from Paillier import Paillier
+from ElGamal import ElGamal
+from ECC import ECC
 import io
 import re
+import ast
 
 app = Flask(__name__)
+
+ecc = ECC()
 
 def display5LetterGroup(text):
     result = re.findall('.{1,5}', text)
@@ -80,6 +85,76 @@ def paillierDecrypt():
         return render_template("paillier.html", mode="Decrypt", g=g, n=n, lambdaa=lambdaa, mu=mu, ciphertext=text, result=result)
     else:
         return render_template("paillier.html", mode="Decrypt")
+
+@app.route("/elgamal/generate", methods=['POST','GET'])
+def elGamalGenerate():
+    if request.method == 'POST':
+        key = ElGamal.generateKeys()
+        public_key = key[0]
+        private_key = key[1]
+        return render_template("elgamal.html", mode="Generate", public = public_key, private = private_key)
+    else:
+        return render_template("elgamal.html", mode="Generate")
+
+@app.route("/elgamal/encrypt", methods=['POST','GET'])
+def elGamalEncrypt():
+    if request.method == 'POST':
+        y = int(request.form['y'])
+        g = int(request.form['g'])
+        p = int(request.form['p'])
+        text = request.form['text']
+        result = ElGamal.encrypt(text, (y, g, p))
+        return render_template("elgamal.html", mode="Encrypt", y=y, g=g, p=p, plaintext=text, result=result)
+    else:
+        return render_template("elgamal.html", mode="Encrypt")
+
+@app.route("/elgamal/decrypt", methods=['POST','GET'])
+def elGamalDecrypt():
+    if request.method == 'POST':
+        x = int(request.form['x'])
+        p = int(request.form['p'])
+        text = ast.literal_eval(request.form['text'])
+        result = ElGamal.decrypt(text, (x, p))
+        return render_template("elgamal.html", mode="Decrypt", x=x, p=p, ciphertext=text, result=result)
+    else:
+        return render_template("elgamal.html", mode="Decrypt")
+
+@app.route("/ecc/generate", methods=['POST','GET'])
+def eccGenerate():
+    if request.method == 'POST':
+        key = ecc.generateKeys()
+        public_key = key[1]
+        private_key = key[0]
+        return render_template("ecc.html", mode="Generate", public = public_key, private = private_key)
+    else:
+        return render_template("ecc.html", mode="Generate")
+
+@app.route("/ecc/encrypt", methods=['POST','GET'])
+def eccEncrypt():
+    if request.method == 'POST':
+        x = int(request.form['x'])
+        y = int(request.form['y'])
+        text = request.form['text']
+        try:
+            result = (ecc.encrypt(text, (x, y)))
+            return render_template("ecc.html", mode="Encrypt", x=x, y=y, plaintext=text, result=result)
+        except Exception:
+            return render_template("ecc.html", mode="Decrypt", x=x, y=y, ciphertext=text, error="Error in encrypting text")
+    else:
+        return render_template("ecc.html", mode="Encrypt")
+
+@app.route("/ecc/decrypt", methods=['POST','GET'])
+def eccDecrypt():
+    if request.method == 'POST':
+        a = int(request.form['a'])
+        text = ast.literal_eval(request.form['text'])
+        try:
+            result = ecc.decrypt(text, a)
+            return render_template("ecc.html", mode="Decrypt", a=a, ciphertext=text, result=result)
+        except Exception:
+            return render_template("ecc.html", mode="Decrypt", a=a, ciphertext=text, error="Error in decrypting text")
+    else:
+        return render_template("ecc.html", mode="Decrypt")
 
 @app.route("/saveresult", methods=['POST'])
 def saveResult():
